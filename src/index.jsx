f5173c22682bb0f9eb8e97b001c8ea40c945f874
Boatsy007 <rr.hinchcliffe@gmail.com>
2026-06-17 19:36:25 +1000

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import '../styles.css';
 import App from './App.jsx';
 
 const root = createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
